4922ead7e1f6085e9539736071a8758fd3e009f5
learnwithsharif <muhammadshareefujjan03@gmail.com>
2026-07-03 12:28:22 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function showMessage(message) {
+    alert(message);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Learn with Sharif website loaded successfully!");
+});
